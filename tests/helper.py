7b14a321965d4afb511e9f19c2f5b8e6f95e2745
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-27 20:41:23 +0700

    add PUT and DELETE

--- tests/helper.py
+++ tests/helper.py
@@ -7,24 +7,48 @@ port = 3030
 base_url = hostname + ':' + str(port)
 
 def get(path: str, data: object = None):
-  # root
-  if len(path) == 0:
-    return requests.get(base_url)
-
-  url = base_url + path if path[0] == '/' else base_url + '/' + path
   data_string = json.dumps(data)
 
-  return requests.get(url, data=data)
-
-def post(path: str, data: object = None):
   # Root
   if len(path) == 0:
-    return requests.post(base_url)
+    return requests.get(base_url, data=data_string)
 
   url = base_url + path if path[0] == '/' else base_url + '/' + path
+
+  return requests.get(url, data=data_string)
+
+def post(path: str, data: object = None):
   data_string = json.dumps(data)
 
+  # Root
+  if len(path) == 0:
+    return requests.post(base_url, data=data_string)
+
+  url = base_url + path if path[0] == '/' else base_url + '/' + path
+
   return requests.post(url, data=data_string)
+
+def post(path: str, data: object = None):
+  data_string = json.dumps(data)
+
+  # Root
+  if len(path) == 0:
+    return requests.put(base_url, data=data_string)
+
+  url = base_url + path if path[0] == '/' else base_url + '/' + path
+
+  return requests.put(url, data=data_string)
+
+def post(path: str, data: object = None):
+  data_string = json.dumps(data)
+
+  # Root
+  if len(path) == 0:
+    return requests.delete(base_url, data=data_string)
+
+  url = base_url + path if path[0] == '/' else base_url + '/' + path
+
+  return requests.delete(url, data=data_string)
 
 def log_success(*msg):
   print('✔️:', *msg)
