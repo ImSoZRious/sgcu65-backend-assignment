eb996070158a945493b0253f36a10ad6f3af5180
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-28 02:25:22 +0700

    formated file

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,10 @@ mod schema;
 
 #[get("/")]
 fn hello() -> &'static str {
-    "Hello, world!"
+  "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::model::User;
-
-    use diesel::pg::PgConnection;
-    use diesel::prelude::*;
-
-    #[test]
-    fn test() {
-        let database_url = "postgres://postgres:trust@localhost:12345/isd";
-        let conn = PgConnection::establish(&database_url)
-            .expect(&format!("Error connecting to {}", database_url));
-
-        let users = User::get_all_users(&conn);
-        println!("{:?}", users);
-    }
+  rocket::build().mount("/", routes![hello])
 }
