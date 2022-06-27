1fb76f4fb7f03e64adcfdc5b3cd424131e01ed1a
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-27 22:28:21 +0700

    add user test

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,14 +1,98 @@
-from helper import get, post, log_failure, log_success
+import helper as h
 import json
 
+SUCESS = 0
+FAILED = 1
+CONNECTION_ERROR = 2
+
+def is_user(data):
+  return 'id' in data and 'email' in data and 'firstname' in data and 'lastname' in data and 'role' in data
+
+def create_test():
+  try:
+    user_payload = {
+      'email': 'abc@gmail.com',
+      'firstname': 'Finding',
+      'lastname': 'Nemo',
+      'role': 'UX/UI Designer'
+    }
+    res = h.post('/user', json.dumps(user_payload))
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res.text)
+
+    if not is_user(res_data):
+      return FAILED
+    
+    return SUCESS
+
+  except:
+    return CONNECTION_ERROR
+
+def read_test():
+  try:
+    res = h.get('/user')
+    if res.status_code != 200:
+      return FAILED
+    res_data = json.dumps(res.text)
+    if len(res_data) < 1:
+      return FAILED
+    test_data = res_data[0]
+    if not is_user(test_data):
+      return FAILED
+
+    return SUCESS
+  except:
+    return CONNECTION_ERROR
+
+def update_test():
+  try:
+    new_data = {
+      'role': 'Backend Developer'
+    }
+    res = h.put('/user/10001', json.dumps(new_data))
+    if res.status_code != 200:
+      return FAILED
+    res_data = json.dumps(res.text)
+    if not (is_user(res_data) and res['role'] == 'Backend Developer'):
+      return FAILED
+
+    return SUCESS  
+  except:
+    return CONNECTION_ERROR
+
+def delete_test():
+  try:
+    res = h.delete('/user/10001')
+    if res.status_code != 200:
+      return FAILED
+    res_data = json.dumps(res.text)
+    if not is_user(res_data):
+      return FAILED
+    return SUCESS
+  except:
+    return CONNECTION_ERROR
+
+def find_test():
+  try:
+    res = h.get('/user/10001')
+    if res.status_code != 200:
+      return FAILED
+    res_data = json.dumps(res.text)
+    if not is_user(res_data):
+      return FAILED
+
+    return SUCESS
+  except:
+    return CONNECTION_ERROR
+
 def user_test():
-  # Create user
+  tests_name = ['Create', 'Read', 'Update', 'Delete', 'Find']
+  tests_func = [create_test, read_test, update_test, delete_test, find_test]
 
-  # Read all user
+  eval_func = {0: h.log_success, 1: h.log_failure, 2: h.log_connection_error}
 
-  # Update user
-
-  # Delete user
-
-  # Find user
-  pass
+  for test_name, test_func in zip(tests_name, tests_func):
+    eval_func[test_func()](test_name)
