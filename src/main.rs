d851b9bf1a6d264549c7fd48e75f417afa794a55
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-28 02:06:24 +0700

    change rocket.rs version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate diesel;
+
+mod model;
+mod schema;
 
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::model::User;
+
+    use diesel::pg::PgConnection;
+    use diesel::prelude::*;
+
+    #[test]
+    fn test() {
+        let database_url = "postgres://postgres:trust@localhost:12345/isd";
+        let conn = PgConnection::establish(&database_url)
+            .expect(&format!("Error connecting to {}", database_url));
+
+        let users = User::get_all_users(&conn);
+        println!("{:?}", users);
+    }
 }
