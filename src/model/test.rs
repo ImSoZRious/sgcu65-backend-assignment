c269ae246e851f439fb74c726a0986f339025124
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-28 18:12:36 +0700

    add task tests

--- src/model/test.rs
+++ src/model/test.rs
@@ -1,5 +1,10 @@
+// this module doesn't count as compilation unit, so it doesn't impact performance
+// of binary in production
+#[allow(dead_code)]
+#[allow(unused_imports)]
 use std::env;
 
+use super::task::{NewTask, Task, UpdateTask};
 use super::user::{NewUser, UpdateUser, User};
 
 use diesel::pg::PgConnection;
@@ -83,4 +88,59 @@ fn find_user() {
   let user_id = 3;
 
   let _user = User::find(user_id, &conn).unwrap();
+}
+
+#[test]
+fn get_all_task() {
+  let conn = get_db_con();
+
+  let _users = Task::get_all(&conn).unwrap();
+}
+
+// Task test
+#[test]
+fn create_task() {
+  let conn = get_db_con();
+
+  let new_task = NewTask {
+    name: s!("Comprog"),
+    content: s!("Grader"),
+    status: s!("In Progress"),
+    deadline: s!("2022-10-31 12:00:00"),
+  };
+
+  let _task = Task::create(new_task, &conn).unwrap();
+}
+
+#[test]
+fn update_task() {
+  let conn = get_db_con();
+
+  let update_info = UpdateTask {
+    id: 1,
+    name: None,
+    content: Some(s!("EZ Comprog")),
+    status: Some(s!("Done")),
+    deadline: None,
+  };
+
+  let _result = Task::update(update_info, &conn).unwrap();
+}
+
+#[test]
+fn delete_task() {
+  let conn = get_db_con();
+
+  let delete_id = 3;
+
+  let _del_count = Task::delete(delete_id, &conn).unwrap();
+}
+
+#[test]
+fn find_task() {
+  let conn = get_db_con();
+
+  let task_id = 3;
+
+  let _task = Task::find(task_id, &conn).unwrap();
 }
