f276340efee7b8f1512f82135776a456b7286193
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-29 03:26:06 +0700

    change test to meet specification

--- tests/task_test.py
+++ tests/task_test.py
@@ -5,10 +5,13 @@ SUCESS = 0
 FAILED = 1
 CONNECTION_ERROR = 2
 
+create_id = None
+
 def is_task(data):
   return 'name' in data and 'content' in data and 'deadline' in data and 'status' in data and 'id' in data
 
 def create_test():
+  global create_id
   try:
     task_payload = {
       'name': 'assignment',
@@ -26,6 +29,8 @@ def create_test():
     if not is_task(res_data):
       return FAILED
     
+    create_id = str(res_data['id'])
+
     return SUCESS
 
   except:
@@ -52,24 +57,17 @@ def update_test():
     new_data = {
       'deadline': '2022-07-30 00:00:00'
     }
-    res = h.put('/task/10001', new_data)
+    res = h.put('/task/' + create_id, new_data)
     if res.status_code != 200:
       return FAILED
-    res_data = json.loads(res.text)
-    if not (is_task(res_data) and res['deadline'] == '2022-07-30 00:00:00'):
-      return FAILED
-
     return SUCESS  
   except:
     return CONNECTION_ERROR
 
 def delete_test():
   try:
-    res = h.delete('/task/10001')
+    res = h.delete('/task/' +  + create_id)
     if res.status_code != 200:
-      return FAILED
-    res_data = json.loads(res.text)
-    if not is_task(res_data):
       return FAILED
     return SUCESS
   except:
@@ -77,11 +75,8 @@ def delete_test():
 
 def find_test():
   try:
-    res = h.get('/task/10001')
+    res = h.get('/task/' + create_id)
     if res.status_code != 200:
-      return FAILED
-    res_data = json.loads(res.text)
-    if not is_task(res_data):
       return FAILED
 
     return SUCESS
@@ -89,10 +84,11 @@ def find_test():
     return CONNECTION_ERROR
 
 def task_test():
-  tests_name = ['Create', 'Read', 'Update', 'Delete', 'Find']
-  tests_func = [create_test, read_test, update_test, delete_test, find_test]
+  tests_name = ['Create', 'Read', 'Update', 'Find', 'Delete']
+  tests_func = [create_test, read_test, update_test, find_test, delete_test]
 
   eval_func = {0: h.log_success, 1: h.log_failure, 2: h.log_connection_error}
 
   for test_name, test_func in zip(tests_name, tests_func):
-    eval_func[test_func()](test_name)
+    if test_name == 'Create' or create_id is not None:
+      eval_func[test_func()](test_name)
--- tests/user_test.py
+++ tests/user_test.py
@@ -5,10 +5,13 @@ SUCESS = 0
 FAILED = 1
 CONNECTION_ERROR = 2
 
+create_id = None
+
 def is_user(data):
   return 'id' in data and 'email' in data and 'firstname' in data and 'lastname' in data and 'role' in data
 
 def create_test():
+  global create_id
   try:
     user_payload = {
       'email': 'abc@gmail.com',
@@ -26,6 +29,8 @@ def create_test():
     if not is_user(res_data):
       return FAILED
     
+    create_id = str(res_data['id'])
+
     return SUCESS
 
   except:
@@ -52,24 +57,17 @@ def update_test():
     new_data = {
       'role': 'Backend Developer'
     }
-    res = h.put('/user/10001', new_data)
+    res = h.put('/user/' + create_id, new_data)
     if res.status_code != 200:
       return FAILED
-    res_data = json.loads(res.text)
-    if not (is_user(res_data) and res['role'] == 'Backend Developer'):
-      return FAILED
-
     return SUCESS  
   except:
     return CONNECTION_ERROR
 
 def delete_test():
   try:
-    res = h.delete('/user/10001')
+    res = h.delete('/user/' + create_id)
     if res.status_code != 200:
-      return FAILED
-    res_data = json.loads(res.text)
-    if not is_user(res_data):
       return FAILED
     return SUCESS
   except:
@@ -77,7 +75,7 @@ def delete_test():
 
 def find_test():
   try:
-    res = h.get('/user/10001')
+    res = h.get('/user/' + create_id)
     if res.status_code != 200:
       return FAILED
     res_data = json.loads(res.text)
@@ -89,10 +87,11 @@ def find_test():
     return CONNECTION_ERROR
 
 def user_test():
-  tests_name = ['Create', 'Read', 'Update', 'Delete', 'Find']
-  tests_func = [create_test, read_test, update_test, delete_test, find_test]
+  tests_name = ['Create', 'Read', 'Update', 'Find', 'Delete']
+  tests_func = [create_test, read_test, update_test, find_test, delete_test]
 
   eval_func = {0: h.log_success, 1: h.log_failure, 2: h.log_connection_error}
 
   for test_name, test_func in zip(tests_name, tests_func):
-    eval_func[test_func()](test_name)
+    if test_name == 'Create' or create_id is not None:
+      eval_func[test_func()](test_name)
