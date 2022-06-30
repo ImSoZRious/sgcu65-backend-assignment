3bcdadc121b09c72d18a866b07e6348bd7854119
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-06-30 21:34:26 +0700

    Add assign test

---
+++ tests/assign_test.py
@@ -0,0 +1,183 @@
+import helper as h
+import json
+
+SUCCESS = 0
+FAILED = 1
+CONNECTION_ERROR = 2
+
+user_id1 = None
+task_id1 = None
+task_id2 = None
+
+def is_user(data):
+  return 'id' in data and 'email' in data and 'firstname' in data and 'lastname' in data and 'role' in data
+
+def is_task(data):
+  return 'name' in data and 'content' in data and 'deadline' in data and 'status' in data and 'id' in data
+
+def create_user():
+  global user_id1
+  try:
+    user_payload = {
+      'email': 'abc@gmail.com',
+      'firstname': 'Finding',
+      'lastname': 'Nemo',
+      'role': 'UX/UI Designer'
+    }
+    res = h.post('/user', user_payload)
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res.text)
+
+    if not is_user(res_data):
+      return FAILED
+    
+    user_id1 = str(res_data['id'])
+
+    return SUCCESS
+
+  except:
+    return CONNECTION_ERROR
+
+def create_task():
+  global task_id1, task_id2
+  try:
+    task = [
+    {
+      'name': 'Comprog',
+      'content': 'Grader',
+      'status': 'Done',
+      'deadline': '2022-10-30 15:00:00'
+    },
+    {
+      'name': 'Calculus',
+      'content': 'Homework',
+      'status': 'In progress',
+      'deadline': '2022-12-15 12:00:33'
+    }]
+    res = h.post('/task', task[0])
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res.text)
+
+    if not is_task(res_data):
+      return FAILED
+    
+    task_id1 = str(res_data['id'])
+    res = h.post('/task', task[1])
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res.text)
+
+    if not is_task(res_data):
+      return FAILED
+    
+    task_id2 = str(res_data['id'])
+
+    return SUCCESS
+
+  except:
+    return CONNECTION_ERROR
+  
+def assign_task():
+  try:
+    assign_obj_1 = {
+      'user_id': user_id1,
+      'task_id': task_id1
+    }
+    assign_obj_2 = {
+      'user_id': user_id1,
+      'task_id': task_id2
+    }
+
+    res = h.post('/assign', assign_obj_1)
+    if res.status_code != 200:
+      return FAILED
+    
+    res = h.post('/assign', assign_obj_2)
+    if res.status_code != 200:
+      return FAILED
+    
+    return SUCCESS
+
+  except:
+    return CONNECTION_ERROR
+
+def get_from_task():
+  try:
+    res = h.get("task/{}/owner".format(task_id1))
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res)
+
+    if not is_user(res_data) or str(res_data['id']) != user_id1:
+      return FAILED
+
+    res = h.get("task/{}/owner".format(task_id2))
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res)
+
+    if not is_user(res_data) or str(res_data['id']) != user_id1:
+      return FAILED
+    return SUCCESS
+
+  except:
+    return CONNECTION_ERROR
+
+def get_from_user():
+  try:
+    res = h.get("user/{}/task".format(user_id1))
+
+    if res.status_code != 200:
+      return FAILED
+
+    res_data = json.loads(res)
+
+    if not all([is_task(task) and str(task['id']) in [task_id1, task_id2] for task in res_data]):
+      return FAILED
+
+    if len(res_data) != 2:
+      return FAILED
+
+    return SUCCESS
+
+  except:
+    return CONNECTION_ERROR
+
+def clean_up():
+  try:
+    res = h.delete("user/{}".format(user_id1))
+    if res.status_code != 200:
+      return FAILED
+    
+    res = h.delete("task/{}".format(task_id1))
+    if res.status_code != 200:
+      return FAILED
+    
+    res = h.delete("task/{}".format(task_id2))
+    if res.status_code != 200:
+      return FAILED
+    return SUCCESS
+  except:
+    return CONNECTION_ERROR
+
+def assign_test():
+  tests_name = ['Create User', 'Create Task', 'Assign Task', 'Check from task', 'Check from user', 'Cleanup']
+  tests_func = [create_user, create_task, assign_task, get_from_task, get_from_user, clean_up]
+
+  eval_func = {0: h.log_success, 1: h.log_failure, 2: h.log_connection_error}
+
+  for test_name, test_func in zip(tests_name, tests_func):
+    if 'Create' in test_name or all([x is not None for x in [user_id1, task_id1, task_id2]]):
+      eval_func[test_func()](test_name)
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 from helper import get, post
 from user_test import user_test
 from task_test import task_test
+from assign_test import assign_test
 
 def main():
   print('[USER TEST]')
@@ -8,6 +9,9 @@ def main():
 
   print('[TASK TEST]')
   task_test()
+
+  print('[ASSIGN TEST]')
+  assign_test()
 
 if __name__ == '__main__':
   main()
